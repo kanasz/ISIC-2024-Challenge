e935d6b4ec6bc3283b74b7e200d6081048fdbe99
Robert Kanasz <kanasz.robert@gmail.com>
2024-07-19 14:52:33 +0200

    DataModule added

---
+++ main.py
@@ -0,0 +1,9 @@
+import h5py
+import numpy as np
+import io
+from PIL import Image
+import matplotlib.pyplot as plt
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("MAIN")
+
