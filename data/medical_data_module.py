a9f6d821dfcc980f0c4de6b8a8341f347af1cab6
Robert Kanasz <kanasz.robert@gmail.com>
2024-08-22 09:23:50 +0200

    minor changes

--- data/medical_data_module.py
+++ data/medical_data_module.py
@@ -8,41 +8,57 @@ from PIL import Image
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset, Subset
 
+hdf5_file = None
+#Very ugly temporary fix
+hdf5_file_path_global = "_raw_data/train-image.hdf5"
+
+
+def worker_init_fn(worker_id):
+    global hdf5_file
+    global hdf5_file_path_global
+    hdf5_file = h5py.File(hdf5_file_path_global, 'r')
+
 
 class MedicalDataset(Dataset):
-    def __init__(self, images, metadata, transformations=None, hdf5_file_path=None):
-        self.images = images
+    def __init__(self, metadata, transformations=None, hdf5_file_path=None):
+        #self.images = images
         self.metadata = metadata
         self.transformations = transformations
+
         self.hdf5_file_path = hdf5_file_path
+        #self.hdf5_file = h5py.File(self.hdf5_file_path, 'r')
 
     def __len__(self):
         return self.metadata.shape[0]
 
     def __getitem__(self, idx):
-        with h5py.File(self.hdf5_file_path, 'r') as f:
-            #df_selected = self.metadata[self.metadata['isic_id']].iloc[idx]
-            df_selected = self.metadata.iloc[idx]
-            #df_selected = self.metadata[self.metadata['isic_id']==idx]
-            label = df_selected['target']
-            isic_id = df_selected['isic_id']
+        global hdf5_file
 
-            image = Image.open(BytesIO(f[isic_id][()]))
-            #image = image.convert('RGB')
-            image = np.array(image)
-            #image = (image - image.min()) / (image.max() - image.min() + 1e-6) * 255
+        #with h5py.File(self.hdf5_file_path, 'r') as f:
+        #df_selected = self.metadata[self.metadata['isic_id']].iloc[idx]
+        df_selected = self.metadata.iloc[idx]
+        #df_selected = self.metadata[self.metadata['isic_id']==idx]
+        label = df_selected['target']
+        isic_id = df_selected['isic_id']
 
-            if self.transformations:
-                image = self.transformations(image=image)['image']
+        image = Image.open(BytesIO(hdf5_file[isic_id][()]))
 
-            #image = image / 255.0  # Normalize image
+        #image = image.convert('RGB')
+        image = np.array(image)
+
+        if self.transformations:
+            image = self.transformations(image=image)['image']
 
         return image, label
 
 
 class MedicalDataModule(pl.LightningDataModule):
-    def __init__(self, image_path, metadata_path, batch_size=32, split_ratio=0.8, subset_ratio=0.5, transforms=None, training_minority_oversampling_ceoff=5):
+    def __init__(self, image_path, metadata_path, batch_size=32, split_ratio=0.8, subset_ratio=0.5, transforms=None,
+                 training_minority_oversampling_ceoff=5):
         super().__init__()
+        global hdf5_file_path_global
+        hdf5_file_path_global = image_path
+
         self.image_path = image_path
         self.metadata_path = metadata_path
         self.batch_size = batch_size
@@ -55,15 +71,17 @@ class MedicalDataModule(pl.LightningDataModule):
         self.majority_indices = None
         self.df_metadata = None
         self.pos_weight = torch.tensor([100], device="cuda")
-        self.cls_num_list = torch.tensor([],device="cuda")
+        self.cls_num_list = torch.tensor([], device="cuda")
         self.training_minority_oversampling_ceoff = training_minority_oversampling_ceoff
+
+
 
     def setup(self, stage=None):
         self.df_metadata = pd.read_csv(self.metadata_path, index_col=False)
-        f = h5py.File(self.image_path, 'r')
+        #f = h5py.File(self.image_path, 'r')
         # Extract necessary information
 
-        self.df_metadata = self.df_metadata[self.df_metadata['tbp_lv_dnn_lesion_confidence']>95]
+        self.df_metadata = self.df_metadata[self.df_metadata['tbp_lv_dnn_lesion_confidence'] > 95]
         self.df_metadata = self.df_metadata.reset_index()
 
         patient_ids = self.df_metadata['patient_id'].values
@@ -91,18 +109,18 @@ class MedicalDataModule(pl.LightningDataModule):
         # Identify minority and majority class samples
         minority_class = self.df_metadata['target'].value_counts().idxmin()
         self.minority_indices = self.df_metadata[(self.df_metadata.index.isin(train_indices_all)) & (
-                    self.df_metadata['target'] == minority_class)].index.tolist()
+                self.df_metadata['target'] == minority_class)].index.tolist()
 
         self.minority_indices = self.minority_indices * self.training_minority_oversampling_ceoff
 
         self.majority_indices = self.df_metadata[(self.df_metadata.index.isin(train_indices_all)) & (
-                    self.df_metadata['target'] != minority_class)].index.tolist()
+                self.df_metadata['target'] != minority_class)].index.tolist()
 
         self.minority_val_indices = self.df_metadata[(self.df_metadata.index.isin(self.val_indices)) & (
-                    self.df_metadata['target'] == minority_class)].index.tolist()
+                self.df_metadata['target'] == minority_class)].index.tolist()
 
         self.majority_val_indices = self.df_metadata[(self.df_metadata.index.isin(self.val_indices)) & (
-                    self.df_metadata['target'] != minority_class)].index.tolist()
+                self.df_metadata['target'] != minority_class)].index.tolist()
 
         # Debugging: Print information about indices
         print(f"Total training indices: {len(train_indices_all)}")
@@ -111,10 +129,13 @@ class MedicalDataModule(pl.LightningDataModule):
         print(f"Total validation indices: {len(self.val_indices)}")
         print(f"Minority validation indices: {len(self.minority_val_indices)}")
 
-        self.pos_weight = torch.tensor([(int(len(self.majority_indices) * self.subset_ratio)) / len(self.minority_indices)], device="cuda")
-        self.cls_num_list = torch.tensor([len(self.majority_indices) * self.subset_ratio, len(self.minority_indices)], device="cuda")
+        self.pos_weight = torch.tensor(
+            [(int(len(self.majority_indices))) / len(self.minority_indices)], device="cuda")
+        #self.pos_weight = torch.tensor([(int(len(self.majority_indices) * self.subset_ratio)) / len(self.minority_indices)], device="cuda")
+        self.cls_num_list = torch.tensor([len(self.majority_indices) * self.subset_ratio, len(self.minority_indices)],
+                                         device="cuda")
 
-        self.val_dataset = Subset(MedicalDataset(f, self.df_metadata, transformations=self.transforms['validation'],
+        self.val_dataset = Subset(MedicalDataset(self.df_metadata, transformations=self.transforms['validation'],
                                                  hdf5_file_path=self.image_path),
                                   self.val_indices)
 
@@ -139,12 +160,14 @@ class MedicalDataModule(pl.LightningDataModule):
 
         #self.pos_weight = torch.tensor([len(sampled_majority_indices) / len(self.minority_indices)], device="cuda")
         # Update the train dataset with the new subset
-        self.train_dataset = Subset(MedicalDataset(f, self.df_metadata, transformations=self.transforms['train'],
+        self.train_dataset = Subset(MedicalDataset(self.df_metadata, transformations=self.transforms['train'],
                                                    hdf5_file_path=self.image_path), sampled_train_indices)
 
     def train_dataloader(self):
         self.update_train_dataset()  # Update dataset at each epoch
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4 , persistent_workers=True,
+                          worker_init_fn=worker_init_fn)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=6, persistent_workers=True,
+                          worker_init_fn=worker_init_fn)
